b65b00b7f446dec18bc5c12829eafeb2f24287a2
arafatahmedtanimcsedu57 <arafatahmedtanimcsedu57@gmail.com>
2022-02-10 17:44:42 +0600

    update

--- services/db_services/dbUserService.js
+++ services/db_services/dbUserService.js
@@ -19,6 +19,7 @@ const getUserTeam = async function (params) {
     const teams = User
         .findById(userId)
         .select('teams')
+        .populate('teams')
         .then(teams => teams)
         .catch(err => console.log(err));
     
