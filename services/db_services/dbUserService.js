9b574a7132bba5cc369ae09f7607752ee7671335
Arafat Ahmed <73300439+olla-world@users.noreply.github.com>
2022-02-13 01:50:23 +0600

    Update dbUserService.js

--- services/db_services/dbUserService.js
+++ services/db_services/dbUserService.js
@@ -19,7 +19,8 @@ const getUserTeam = async function (params) {
     const teams = User
         .findById(userId)
         .select('teams')
-        .populate('teams')
+        .populate('members')
+        .populate('admin')
         .then(teams => teams)
         .catch(err => console.log(err));
     
